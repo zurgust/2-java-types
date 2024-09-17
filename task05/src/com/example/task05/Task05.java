793b1cbc6642240b954b161a4c39ca7ee557d453
Zakharov Danila <danila.zaxarov.2015@gmail.com>
2024-09-17 11:03:17 +0300

    Захаров Данила. Задание 05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,22 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        String inputX = Integer.toString(x);
+        int length = inputX.length();
+        while (length > 1) {
+            if (x % 2 != 0) {
+                return "FALSE";
+            }
+            length--;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
